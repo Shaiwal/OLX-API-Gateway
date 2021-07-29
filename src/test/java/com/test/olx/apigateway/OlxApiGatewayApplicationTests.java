5d747a9d8eb12e671c8ad8849034000c985e87da
Shaiwal Sharma -X (shaiwsha - ZENSAR TECHNOLOGIES INC at Cisco) <shaiwsha@partnet.cisco.com>
2021-07-29 15:39:36 +0530

    First Commit -m API Gateway

---
+++ src/test/java/com/test/olx/apigateway/OlxApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.olx.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OlxApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
